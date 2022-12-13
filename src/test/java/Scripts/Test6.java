6af4c402e8d1dd153f0dc8d74aa6146454ba2e9c
HP <HP@DESKTOP-424KU7A>
2022-12-13 20:00:05 +0530

    hi

---
+++ src/test/java/Scripts/Test6.java
@@ -0,0 +1,17 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test6 {
+
+	@Test
+	public void tc6()
+	{
+		Reporter.log("Hello API11",true);
+	}
+	
+	
+	
+	
+}
